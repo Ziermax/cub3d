26679e71d10f540f8ac0313b682cbaa8ed65b58c
gabsanch <130669207+gasanche@users.noreply.github.com>
2024-09-02 17:16:11 +0200

    Errors checked: Wrong number of parameters, wrong file type, file is ocult, file has no read permissions, file cannot be opened

--- sources/main.c
+++ sources/main.c
@@ -15,21 +15,56 @@
 #include "../includes/cub3d.h"
 #include <math.h>
 
+int	open_file(char *argv, int *fd)
+{
+	if (access(argv, R_OK) == -1)
+		return (-1);
+	*fd = open(argv, O_RDONLY);
+	return (*fd);
+}
+
+int	is_ocult(char *argv)
+{
+	//int		i;
+	char	*checker;
+
+	checker = strrchr(argv, '/');
+	if (checker && strncmp(checker + 1, ".", 1) == 0)
+		return (-1);
+	if (!checker && strncmp(argv, ".", 1) == 0)
+		return (-1);
+	return (0);
+}
+
+int	check_file(char *argv)
+{
+	int	i;
+
+	i = strlen(argv) - 4;
+	if (i > 0)
+	{
+		if (is_ocult(argv) == 0 && strncmp(argv + i, ".cub", 4) == 0)
+			return (0);
+	}
+	return (-1);
+}
+
 int	main(int argc, char **argv)
 {
-	t_data	data;
-	t_mlx	mlx;
-	t_img	img;
-	t_map	map;
-
-	(void)argv;
+	int	fd;
+	
 	if (argc == 2)
-		return (fd_printf(2, "Wrong number of arguments\n"), 1);
-	fd_printf(1, "Estarting CubTresD\n");
-	data_init(&data, &mlx, &img, &map);
-	if (!data.mlx)
-		return (fd_printf(2, "Failed mlx_functions\n"));
-	mlx_loop(data.mlx->ptr);
-	fd_printf(1, "Finished Cub2d\n");
-	return (data.value);
+	{
+		//pon un error si un pelotudo te pasa un .cub a secas
+		if (check_file(argv[1]) == -1)
+			exit(printf("Error\nWrong file!\nOnly .cub please :)\nPS: We don't accept occulted files uwu\n"));
+		if (open_file(argv[1], &fd) == -1))
+			exit(printf("Error\nCouldn't open file :(\n Check if the path is correct and file has read permissions\n");
+		
+	}
+	else
+	{
+		printf("Error\nWrong number of arguments\n");
+		exit(1);
+	}
 }
