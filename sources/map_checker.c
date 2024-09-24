b6472c15222ede81e5f4e4c635c833161a5ee00a
gabsanch <130669207+gasanche@users.noreply.github.com>
2024-09-24 13:00:59 +0200

    Update map_checker.c

--- sources/map_checker.c
+++ sources/map_checker.c
@@ -56,41 +56,33 @@ int parse_map(int fd, t_map *map) {
     char *line;
     int index;
     int player_found;
-    int line_count;
+    //int line_count; a lo mejor la necesito luego
     int map_ended;
 
     layout = NULL;
     map_ended = 0;
     player_found = 0;
     index = 0;
-
-    // inicio layout pero aun no sé cuantas líneas de mapa
-    layout = (char **)malloc(sizeof(char *));
-    if (!layout) {
-        fd_printf(2, "Error\n Memory allocation failed\n");
-        return (-1);
-    }
-    //preguntarle a maxi si hay alguna manera de saber cuantas líneas habrá sin leer el fd
-    //    line_count = ; para saber las líneas que habrá
-
+   
     // leer el archivo y almacenar las líneas en layout
     line = get_next_line(fd);
     while (line)
     {
-        if (strlen(line) == 0 && map_ended == 0)
+        
+        if (ft_strlen(line) == 0 && map_ended == 0)
         {
             free(line);  // skip empty lines
             line = get_next_line(fd);
             continue;
         }
-        if (strlen(line) == 0 && map_ended == 1)
+        if (ft_strlen(line) == 0 && map_ended == 1)
         {
             printf("Error: Map is not closed.\n");
             return -1;
         }
         layout[index] = line;
 
-        // miramos la posisción del jugador
+        // miramos la posición del jugador
         int i = 0;
         while (layout[index][i])
         {
